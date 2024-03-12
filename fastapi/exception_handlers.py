2912986a826e9aa1924f065ec379534a62180901
chanelcolgate <57675195+chanelcolgate@users.noreply.github.com>
2024-03-12 09:00:34 +0700

    Update exception_handlers.py

--- fastapi/exception_handlers.py
+++ fastapi/exception_handlers.py
@@ -13,7 +13,7 @@ async def http_exception_handler(request: Request, exc: HTTPException) -> Respon
     if not is_body_allowed_for_status_code(exc.status_code):
         return Response(status_code=exc.status_code, headers=headers)
     return JSONResponse(
-        {"detail": exc.detail}, status_code=exc.status_code, headers=headers
+        {"detail": exc.detail, "result": exc.result}, status_code=exc.status_code, headers=headers
     )
 
 
