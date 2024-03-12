5bc49bd8b05ac33ce1ffad468a0d27bca0018b5d
chanelcolgate <57675195+chanelcolgate@users.noreply.github.com>
2024-03-12 09:48:49 +0700

    Update exception_handlers.py

--- fastapi/exception_handlers.py
+++ fastapi/exception_handlers.py
@@ -22,8 +22,7 @@ async def request_validation_exception_handler(
 ) -> JSONResponse:
     return JSONResponse(
         status_code=HTTP_422_UNPROCESSABLE_ENTITY,
-        content={"detail": jsonable_encoder(exc.errors())},
-        result="fail",
+        content={"detail": jsonable_encoder(exc.errors()), "result": "fail"},
     )
 
 
